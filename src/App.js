3809d853836ff55d0869ddae8276398c507acfdc
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-09-12 11:47:57 +0800

    Added a form and functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./components/TodoItem";
+import TodoForm from "./components/TodoForm";
+
 
 
 function App() {
+//variables
+
+const [todos, setTodos] = useState([
+  "Finish plus project",
+  "Meal prep",
+  "Sleep",
+]);
+
+
+//methods
+const addTodo = (text) => {
+  const newTodos = [...todos, text];
+  setTodos(newTodos);
+};
+
+
+
+  //template
   return (
     <div>
       <h1>My todo list</h1>
-      <TodoItem todo="Finish plus project." />
-      <TodoItem todo="Meal prep." />
-      <TodoItem todo="Sleep." />
-    </div>
+      {todos.map((todo, index) => (
+        <TodoItem todo={todo} key={index} />    
+      ))}
+      <TodoForm addTodo={addTodo} />
+    
+    </div>   
+      // <TodoItem todo="Finish plus project." />
+      // <TodoItem todo="Meal prep." />
+      // <TodoItem todo="Sleep." />
   );
 }
 
